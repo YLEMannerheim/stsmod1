9e0da8abfd0ee1b545c7379b004d46e9c4c6d993
MaybeLaterx <MaybeLater_x@hotmail.co.uk>
2022-12-20 23:54:02 +0000

    spare bucket working

--- BasicMod-master/BasicMod-master/src/main/java/dannyandjannymod/powers/SpareBucketPower.java
+++ BasicMod-master/BasicMod-master/src/main/java/dannyandjannymod/powers/SpareBucketPower.java
@@ -16,62 +16,24 @@ import com.megacrit.cardcrawl.powers.AbstractPower;
 import dannyandjannymod.CustomTags;
 import dannyandjannymod.util.TextureLoader;
 
+import static dannyandjannymod.BasicMod.makeID;
+
 public class SpareBucketPower extends AbstractPower {
-    private static PowerStrings getPowerStrings(String ID)
-    {
-        return CardCrawlGame.languagePack.getPowerStrings(ID);
+
+    public SpareBucketPower() {
+        this.name = "Spare Bucket";
+        this.ID = makeID("Spare Bucket");
+        this.owner = AbstractDungeon.player;
+        this.updateDescription();
+        this.loadRegion("confusion");
     }
-    protected AbstractCreature source;
-    protected String[] DESCRIPTIONS;
 
-    public SpareBucketPower(String id, PowerType powerType, boolean isTurnBased, AbstractCreature owner, int amount) {
-        this(id, powerType, isTurnBased, owner, null, amount);
+    public void updateDescription() {
+        description = "Bucket cards are played twice.";
     }
-    public SpareBucketPower(String id, PowerType powerType, boolean isTurnBased, AbstractCreature owner, AbstractCreature source, int amount) {
-        this(id, powerType, isTurnBased, owner, source, amount, true);
-    }
-    public SpareBucketPower(String id, PowerType powerType, boolean isTurnBased, AbstractCreature owner, AbstractCreature source, int amount, boolean initDescription) {
-        this(id, powerType, isTurnBased, owner, source, amount, initDescription, true);
-    }
-    public SpareBucketPower(String id, PowerType powerType, boolean isTurnBased, AbstractCreature owner, AbstractCreature source, int amount, boolean initDescription, boolean loadImage) {
-        this.ID = id;
-        this.isTurnBased = isTurnBased;
 
-        PowerStrings strings = getPowerStrings(this.ID);
-        this.name = strings.NAME;
-        this.DESCRIPTIONS = strings.DESCRIPTIONS;
-
-        this.owner = owner;
-        this.source = source;
-        this.amount = amount;
-        this.type = powerType;
-
-        if (loadImage)
-        {
-            String unPrefixed = id.substring(id.indexOf(":") + 1);
-            Texture normalTexture = TextureLoader.getPowerTexture(unPrefixed);
-            Texture hiDefImage = TextureLoader.getHiDefPowerTexture(unPrefixed);
-            if (hiDefImage != null)
-            {
-                region128 = new TextureAtlas.AtlasRegion(hiDefImage, 0, 0, hiDefImage.getWidth(), hiDefImage.getHeight());
-                if (normalTexture != null)
-                    region48 = new TextureAtlas.AtlasRegion(normalTexture, 0, 0, normalTexture.getWidth(), normalTexture.getHeight());
-            }
-            else if (normalTexture != null)
-            {
-                this.img = normalTexture;
-                region48 = new TextureAtlas.AtlasRegion(normalTexture, 0, 0, normalTexture.getWidth(), normalTexture.getHeight());
-            }
-        }
-
-        if (initDescription)
-            this.updateDescription();
-
-
-
-    }
     public void onUseCard(AbstractCard card, UseCardAction action) {
-        if (!card.purgeOnUse && card.hasTag(CustomTags.BUCKET) && this.amount > 0) {
+        if (!card.purgeOnUse && card.hasTag(CustomTags.BUCKET)) {
             this.flash();
             AbstractMonster m = null;
             if (action.target != null) {
@@ -82,7 +44,7 @@ public class SpareBucketPower extends AbstractPower {
             AbstractDungeon.player.limbo.addToBottom(tmp);
             tmp.current_x = card.current_x;
             tmp.current_y = card.current_y;
-            tmp.target_x = (float) Settings.WIDTH / 2.0F - 300.0F * Settings.scale;
+            tmp.target_x = (float)Settings.WIDTH / 2.0F - 300.0F * Settings.scale;
             tmp.target_y = (float)Settings.HEIGHT / 2.0F;
             if (m != null) {
                 tmp.calculateCardDamage(m);
@@ -91,6 +53,5 @@ public class SpareBucketPower extends AbstractPower {
             tmp.purgeOnUse = true;
             AbstractDungeon.actionManager.addCardQueueItem(new CardQueueItem(tmp, m, card.energyOnUse, true, true), true);
         }
-
     }
 }
