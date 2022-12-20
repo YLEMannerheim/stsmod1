310c0a78ea480f9aa7cef147331c6ac248b3e3b2
MaybeLaterx <MaybeLater_x@hotmail.co.uk>
2022-12-20 22:52:18 +0000

    sparebucket

---
+++ BasicMod-master/BasicMod-master/src/main/java/dannyandjannymod/powers/SpareBucketPower.java
@@ -0,0 +1,96 @@
+package dannyandjannymod.powers;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardQueueItem;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import dannyandjannymod.CustomTags;
+import dannyandjannymod.util.TextureLoader;
+
+public abstract class SpareBucketPower extends AbstractPower {
+    private static PowerStrings getPowerStrings(String ID)
+    {
+        return CardCrawlGame.languagePack.getPowerStrings(ID);
+    }
+    protected AbstractCreature source;
+    protected String[] DESCRIPTIONS;
+
+    public SpareBucketPower(String id, PowerType powerType, boolean isTurnBased, AbstractCreature owner, int amount) {
+        this(id, powerType, isTurnBased, owner, null, amount);
+    }
+    public SpareBucketPower(String id, PowerType powerType, boolean isTurnBased, AbstractCreature owner, AbstractCreature source, int amount) {
+        this(id, powerType, isTurnBased, owner, source, amount, true);
+    }
+    public SpareBucketPower(String id, PowerType powerType, boolean isTurnBased, AbstractCreature owner, AbstractCreature source, int amount, boolean initDescription) {
+        this(id, powerType, isTurnBased, owner, source, amount, initDescription, true);
+    }
+    public SpareBucketPower(String id, PowerType powerType, boolean isTurnBased, AbstractCreature owner, AbstractCreature source, int amount, boolean initDescription, boolean loadImage) {
+        this.ID = id;
+        this.isTurnBased = isTurnBased;
+
+        PowerStrings strings = getPowerStrings(this.ID);
+        this.name = strings.NAME;
+        this.DESCRIPTIONS = strings.DESCRIPTIONS;
+
+        this.owner = owner;
+        this.source = source;
+        this.amount = amount;
+        this.type = powerType;
+
+        if (loadImage)
+        {
+            String unPrefixed = id.substring(id.indexOf(":") + 1);
+            Texture normalTexture = TextureLoader.getPowerTexture(unPrefixed);
+            Texture hiDefImage = TextureLoader.getHiDefPowerTexture(unPrefixed);
+            if (hiDefImage != null)
+            {
+                region128 = new TextureAtlas.AtlasRegion(hiDefImage, 0, 0, hiDefImage.getWidth(), hiDefImage.getHeight());
+                if (normalTexture != null)
+                    region48 = new TextureAtlas.AtlasRegion(normalTexture, 0, 0, normalTexture.getWidth(), normalTexture.getHeight());
+            }
+            else if (normalTexture != null)
+            {
+                this.img = normalTexture;
+                region48 = new TextureAtlas.AtlasRegion(normalTexture, 0, 0, normalTexture.getWidth(), normalTexture.getHeight());
+            }
+        }
+
+        if (initDescription)
+            this.updateDescription();
+
+
+
+    }
+    public void onUseCard(AbstractCard card, UseCardAction action) {
+        if (!card.purgeOnUse && card.hasTag(CustomTags.BUCKET) && this.amount > 0) {
+            this.flash();
+            AbstractMonster m = null;
+            if (action.target != null) {
+                m = (AbstractMonster)action.target;
+            }
+
+            AbstractCard tmp = card.makeSameInstanceOf();
+            AbstractDungeon.player.limbo.addToBottom(tmp);
+            tmp.current_x = card.current_x;
+            tmp.current_y = card.current_y;
+            tmp.target_x = (float) Settings.WIDTH / 2.0F - 300.0F * Settings.scale;
+            tmp.target_y = (float)Settings.HEIGHT / 2.0F;
+            if (m != null) {
+                tmp.calculateCardDamage(m);
+            }
+
+            tmp.purgeOnUse = true;
+            AbstractDungeon.actionManager.addCardQueueItem(new CardQueueItem(tmp, m, card.energyOnUse, true, true), true);
+        }
+
+    }
+}
