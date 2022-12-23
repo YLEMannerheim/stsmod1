3b74ac8a516c8f88c4ee3ca7d0dda7834e46c1db
MaybeLaterx <MaybeLater_x@hotmail.co.uk>
2022-12-23 00:15:13 +0000

    multi

---
+++ BasicMod-master/BasicMod-master/src/main/java/dannyandjannymod/cards/MultivitaminsCard.java
@@ -0,0 +1,45 @@
+package dannyandjannymod.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import dannyandjannymod.CustomTags;
+import dannyandjannymod.util.CardInfo;
+
+import static dannyandjannymod.BasicMod.makeID;
+
+public class MultivitaminsCard extends BaseCard {
+    private final static CardInfo cardInfo = new CardInfo(
+            "MultivitaminsCard",
+            1,
+            CardType.ATTACK,
+            CardTarget.ENEMY,
+            CardRarity.RARE,
+            CardColor.RED);
+
+    public static final String ID = makeID(cardInfo.baseId);
+    private static final int DAMAGE = 1;
+    private static final int MAGIC = 6;
+
+    public MultivitaminsCard() {
+        super(cardInfo);
+        setDamage(DAMAGE);
+        setMagic(MAGIC);
+        setSelfRetain(false, true);
+        setExhaust(true);
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        for (int i = 0; i < this.magicNumber; i++) {
+            addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.FIRE));
+        }
+    }
+
+    @Override
+    public AbstractCard makeCopy() { //Optional
+        return new MultivitaminsCard();
+    }
+}
