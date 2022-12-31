a62d88f240a815889eba93d9ee14835413f2482e
MaybeLaterx <MaybeLater_x@hotmail.co.uk>
2022-12-31 01:55:13 +0000

    Status pictuers (not hooked up)

--- BasicMod-master/BasicMod-master/src/main/java/dannyandjannymod/powers/CalciumComedownPower.java
+++ BasicMod-master/BasicMod-master/src/main/java/dannyandjannymod/powers/CalciumComedownPower.java
@@ -1,11 +1,14 @@
 package dannyandjannymod.powers;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.powers.*;
+import dannyandjannymod.util.TextureLoader;
 
 public class CalciumComedownPower extends AbstractPower {
     public static final String POWER_ID = "CalciumComedownPower";
@@ -28,6 +31,23 @@ public class CalciumComedownPower extends AbstractPower {
         if (this.amount <= -999) {
             this.amount = -999;
         }
+
+
+        String unPrefixed = ID;
+        Texture normalTexture = TextureLoader.getPowerTexture(unPrefixed);
+        Texture hiDefImage = TextureLoader.getHiDefPowerTexture(unPrefixed);
+        if (hiDefImage != null)
+        {
+            region128 = new TextureAtlas.AtlasRegion(hiDefImage, 0, 0, hiDefImage.getWidth(), hiDefImage.getHeight());
+            if (normalTexture != null)
+                region48 = new TextureAtlas.AtlasRegion(normalTexture, 0, 0, normalTexture.getWidth(), normalTexture.getHeight());
+        }
+        else if (normalTexture != null)
+        {
+            this.img = normalTexture;
+            region48 = new TextureAtlas.AtlasRegion(normalTexture, 0, 0, normalTexture.getWidth(), normalTexture.getHeight());
+        }
+
 
     }
 
