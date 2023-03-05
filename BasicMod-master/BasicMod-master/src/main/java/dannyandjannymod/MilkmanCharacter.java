25bda807b69be40d9d853bd08af06d61a53fe21d
MaybeLaterx <MaybeLater_x@hotmail.co.uk>
2023-03-05 22:20:06 +0000

    character update - energy, heart ending, description

--- BasicMod-master/BasicMod-master/src/main/java/dannyandjannymod/MilkmanCharacter.java
+++ BasicMod-master/BasicMod-master/src/main/java/dannyandjannymod/MilkmanCharacter.java
@@ -6,6 +6,7 @@ import basemod.abstracts.CustomPlayer;
 import basemod.animations.SpineAnimation;
 import basemod.animations.SpriterAnimation;
 import basemod.interfaces.EditCharactersSubscriber;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
@@ -17,6 +18,7 @@ import static dannyandjannymod.BasicMod.modID;
 import static org.apache.logging.log4j.core.util.Loader.initializeClass;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import com.badlogic.gdx.math.MathUtils;
 import com.esotericsoftware.spine.AnimationState;
@@ -24,31 +26,44 @@ import com.megacrit.cardcrawl.actions.utility.ExhaustAllEtherealAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.EnergyManager;
 import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.cutscenes.CutscenePanel;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.FontHelper;
 import com.megacrit.cardcrawl.helpers.ScreenShake;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 import com.megacrit.cardcrawl.screens.CharSelectInfo;
 import com.megacrit.cardcrawl.unlock.UnlockTracker;
+import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
+import com.megacrit.cardcrawl.vfx.GhostlyFireEffect;
+import com.megacrit.cardcrawl.vfx.RainingGoldEffect;
+import com.megacrit.cardcrawl.vfx.scene.BonfireParticleEffect;
+import com.megacrit.cardcrawl.vfx.scene.SilentVictoryStarEffect;
 import dannyandjannymod.cards.*;
 import dannyandjannymod.relics.BreakfastRelic;
 
 public class MilkmanCharacter extends CustomPlayer {
-    /*
+
     private static final String[] orbTextures = {
-            modID + "Resources/images/char/mainChar/orb/layer1.png",
-            modID + "Resources/images/char/mainChar/orb/layer2.png",
-            modID + "Resources/images/char/mainChar/orb/layer3.png",
-            modID + "Resources/images/char/mainChar/orb/layer4.png",
-            modID + "Resources/images/char/mainChar/orb/layer5.png",
-            modID + "Resources/images/char/mainChar/orb/layer6.png",
-            modID + "Resources/images/char/mainChar/orb/layer1d.png",
-            modID + "Resources/images/char/mainChar/orb/layer2d.png",
-            modID + "Resources/images/char/mainChar/orb/layer3d.png",
-            modID + "Resources/images/char/mainChar/orb/layer4d.png",
-            modID + "Resources/images/char/mainChar/orb/layer5d.png",};
-     */
-    private static final String[] orbTextures = { "dannyandjannymod/character/characterSelectButton.png" };
+            "dannyandjannymod/character/orb/layer-a.png",
+            "dannyandjannymod/character/orb/layer-b.png",
+            "dannyandjannymod/character/orb/layer-c.png",
+            "dannyandjannymod/character/orb/layer-d.png",
+            "dannyandjannymod/character/orb/layer-a2.png",
+            "dannyandjannymod/character/orb/layer-b.png",
+            "dannyandjannymod/character/orb/layer-c.png"};
+            //"dannyandjannymod/character/orb/layer1z.png",
+            //"dannyandjannymod/character/orb/layer2.png",
+            //"dannyandjannymod/character/orb/layer3.png",
+            //"dannyandjannymod/character/orb/layer4.png",
+            //"dannyandjannymod/character/orb/layer5.png",
+            //"dannyandjannymod/character/orb/layer6z.png",
+            //"dannyandjannymod/character/orb/layer7.png",
+            //"dannyandjannymod/character/orb/layer8.png"};
+            //"dannyandjannymod/character/orb/layer9.png",
+            //"dannyandjannymod/character/orb/layer10.png",
+            //"dannyandjannymod/character/orb/layer11.png"};
+
+    //private static final String[] orbTextures = { "dannyandjannymod/character/characterSelectButton.png" };
     public static final int ENERGY_PER_TURN = 3; // how much energy you get every turn
     public static final String MY_CHARACTER_SHOULDER_2 = "dannyandjannymod/character/shoulder1.png"; // campfire pose
     public static final String MY_CHARACTER_SHOULDER_1 = "dannyandjannymod/character/shoulder1.png"; // another campfire pose
@@ -56,12 +71,15 @@ public class MilkmanCharacter extends CustomPlayer {
     public static final String MY_CHARACTER_SKELETON_ATLAS = "dannyandjannymod/character/MilkmanTest.atlas"; // spine animation atlas
     public static final String MY_CHARACTER_SKELETON_JSON = "dannyandjannymod/character/MilkmanTest.json"; // spine animation json
 
+    public static float update_timer = 0; // heart win effect
+    public static boolean glow_fade = false; // heart win effect
+
     public MilkmanCharacter (String name) {
 
         super(name,
                 MilkmanClassEnum.MILKMAN_WHITE,
                 new CustomEnergyOrb(orbTextures,
-                        "dannyandjannymod/character/char_button.png",
+                        "dannyandjannymod/character/energyOrb2.png",
                         null),
                 //new SpriterAnimation(modID + "Resources/images/char/mainChar/static.scml"));
                 new SpineAnimation(MY_CHARACTER_SKELETON_ATLAS, MY_CHARACTER_SKELETON_JSON, 1f));
@@ -110,7 +128,7 @@ public class MilkmanCharacter extends CustomPlayer {
     public static final int ORB_SLOTS = 0;
 
     public CharSelectInfo getLoadout() { // the rest of the character loadout so includes your character select screen info plus hp and starting gold
-        return new CharSelectInfo("The Milkman", "Who's your daddy?",
+        return new CharSelectInfo("The Milkman", "A sentient carton of milk, brought to life by the Spire. NL Manipulates future turns and battles.",
                 STARTING_HP, MAX_HP, ORB_SLOTS, STARTING_GOLD, HAND_SIZE,
                 this, getStartingRelics(), getStartingDeck(), false);
     }
@@ -184,6 +202,29 @@ public class MilkmanCharacter extends CustomPlayer {
     @Override
     public AbstractGameAction.AttackEffect[] getSpireHeartSlashEffect() {
         return new AbstractGameAction.AttackEffect[]{AbstractGameAction.AttackEffect.SLASH_HEAVY, AbstractGameAction.AttackEffect.SMASH, AbstractGameAction.AttackEffect.BLUNT_HEAVY, AbstractGameAction.AttackEffect.SLASH_HEAVY, AbstractGameAction.AttackEffect.FIRE, AbstractGameAction.AttackEffect.BLUNT_HEAVY};
+    }
+
+    @Override
+    public List<CutscenePanel> getCutscenePanels() {
+        List<CutscenePanel> panels = new ArrayList();
+        panels.add(new CutscenePanel( "dannyandjannymod/character/end/scene1.png", "ATTACK_HEAVY"));
+        panels.add(new CutscenePanel("dannyandjannymod/character/end/scene2.png"));
+        panels.add(new CutscenePanel("dannyandjannymod/character/end/scene3.png"));
+        return panels;
+    }
+
+    @Override
+    public void updateVictoryVfx(ArrayList<AbstractGameEffect> effects) {
+        if (!glow_fade) {
+            effects.add(new BonfireParticleEffect(true, true));
+            glow_fade = true;
+        }
+
+        update_timer += Gdx.graphics.getDeltaTime();
+
+        for (float i = 0; i + (1.0 / 30.0) <= update_timer; update_timer -= (1.0 / 30.0)) {
+            effects.add(new SilentVictoryStarEffect());
+        }
     }
 
     @Override
